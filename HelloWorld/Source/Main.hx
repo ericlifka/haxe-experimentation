5389958d96b3ce481e8c9f0667a461abff302612
Eric Lifka <eric.lifka@gmail.com>
2015-02-15 21:06:05 -0500

    read in the image for use

--- HelloWorld/Source/Main.hx
+++ HelloWorld/Source/Main.hx
@@ -1,18 +1,12 @@
 package;
 
-
 import openfl.display.Sprite;
-
+import openfl.Assets;
 
 class Main extends Sprite {
-	
-	
 	public function new () {
-		
 		super ();
-		
-		
-		
+        var bitmapData = Assets.getBitmapData("assets/openfl.png");
 	}
 
     static public function main() {
