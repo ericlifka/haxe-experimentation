0eb978f3d81492c7b3774a003105d17803ab52df
Eric Lifka <eric.lifka@gmail.com>
2015-02-15 21:09:58 -0500

    load image data into a renderable and add it to the context

--- HelloWorld/Source/Main.hx
+++ HelloWorld/Source/Main.hx
@@ -1,12 +1,16 @@
 package;
 
+import openfl.display.Bitmap;
 import openfl.display.Sprite;
 import openfl.Assets;
 
 class Main extends Sprite {
-	public function new () {
-		super ();
+	public function new() {
+		super();
+
         var bitmapData = Assets.getBitmapData("assets/openfl.png");
+        var bitmap = new Bitmap(bitmapData);
+        addChild(bitmap);
 	}
 
     static public function main() {
